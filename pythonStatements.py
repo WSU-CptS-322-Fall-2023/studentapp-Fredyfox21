cc9ab8534c6830b7c062ca2a7ec70e92449a8599
Fredyfox21 <fredy.fernandez@wsu.edu>
2023-08-23 18:35:05 -0700

    Update pythonStatements.py

--- pythonStatements.py
+++ pythonStatements.py
@@ -4,6 +4,27 @@ from app import db
 #create the database file, if it doesn't exist. 
 db.create_all()
 
+#import Major model
+from app.models import Major
+
+# Create a major
+newMajor = Major(name='CptS',department='school of EECS')
+db.session.add(newMajor)
+newMajor = Major(name='CE',department='Civil Engineering')
+db.session.add(newMajor)
+db.session.commit()
+Major.query.all()
+for m in Major.query.all():
+    print(m)
+
+#Create a class; assign class's major to the major we just created
+from app.models import Class
+NewClass = Class(coursenum='322',major='CptS',title='Software Engineering')
+db.session.add(newClass)
+NewClass = Class(coursenum='315',major='CE',title='Fluid Mechanics')
+db.session.add(newClass)
+
+
 # import db models
 from app.models import Class
 
