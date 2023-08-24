993307cf295b93fb495920af76a10543391aa3c2
Fredyfox21 <fredy.fernandez@wsu.edu>
2023-08-23 18:55:27 -0700

    Update pythonStatements.py

--- pythonStatements.py
+++ pythonStatements.py
@@ -19,26 +19,18 @@ for m in Major.query.all():
 
 #Create a class; assign class's major to the major we just created
 from app.models import Class
-NewClass = Class(coursenum='322',major='CptS',title='Software Engineering')
+newClass = Class(coursenum='322',major='CptS',title='Software Engineering')
 db.session.add(newClass)
-NewClass = Class(coursenum='315',major='CE',title='Fluid Mechanics')
-db.session.add(newClass)
-
-
-# import db models
-from app.models import Class
-
-#create class objects and write them to the database
-newClass = Class(coursenum='322')
-db.session.add(newClass)
-newClass = Class(coursenum='355')
+newClass = Class(coursenum='315',major='CE',title='Fluid Mechanics')
 db.session.add(newClass)
 db.session.commit()
 
-# query and print classes
-Class.query.all()
-Class.query.filter_by(coursenum='322').all()
-Class.query.filter_by(coursenum='322').first()
-myclasses = Class.query.order_by(Class.coursenum.desc()).all()
-for c in myclasses:
-    print(c.coursenum)
+
+allClasses = Class.query.all()
+Class.query.filter_by(major='CptS').all()
+Class.query.filter_by(major='CptS').first()
+Class.query.filter_by(major='CptS').order_by(Class.title).all()
+Class.query.filter_by(major='CptS').count()
+mymajor = Major.query.filter_by(name='CptS').first()
+for c in mymajor.classes:
+    print(c.title)
